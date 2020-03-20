e4725b8972af03386f426363cf1c6ef63cf963b9
Henrique Miranda <enrque.ns@gmail.com>
2020-03-20 14:22:12 -0300

    :bulb: chore: add icon

--- src/components/Links/Information/styles.ts
+++ src/components/Links/Information/styles.ts
@@ -3,16 +3,19 @@ import styled from 'styled-components';
 export const Container = styled.li`
   display: flex;
   align-items: center;
+  justify-content: center;
 
   div {
     margin-left: 48px;
 
     h1 {
-      font-size: 3.6rem;
+      font-size: 2.4rem;
+      max-width: 658px;
     }
 
     p {
       font-size: 1.8rem;
+      max-width: 800px;
       margin: 16px 0 64px;
     }
   }
