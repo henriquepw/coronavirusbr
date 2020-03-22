3dbf4228f7011798c11f2cd7a30c692f351fa02c
Henrique Miranda <enrque.ns@gmail.com>
2020-03-22 11:17:54 -0300

    :lipstick: style: responsive links page

--- src/components/Links/Information/styles.ts
+++ src/components/Links/Information/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import media from '~/styles/media';
+
 export const Container = styled.li`
   display: flex;
   align-items: center;
@@ -25,6 +27,33 @@ export const Container = styled.li`
       font-size: 1.8rem;
       max-width: 800px;
       margin: 16px 0 64px;
+    }
+  }
+
+  @media (max-width: ${media.smallTablet}) {
+    flex-direction: column;
+
+    div:first-of-type {
+      max-width: 90%;
+      margin-bottom: 48px;
+
+      border: 2px solid ${({ theme }) => theme.colors.active};
+    }
+
+    div:nth-of-type(2) {
+      margin-left: 0px;
+      max-width: 90%;
+
+      p {
+        margin-bottom: 32px;
+      }
+    }
+  }
+
+  @media (max-width: ${media.phone}) {
+    div:first-of-type,
+    div:nth-of-type(2) {
+      max-width: 100%;
     }
   }
 `;
